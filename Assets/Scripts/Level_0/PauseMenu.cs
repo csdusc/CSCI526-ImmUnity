0437bc42b65c4e1c1ef74b7ac84de4af4fbff0ca
nmutha <nmutha@usc.edu>
2022-10-30 02:31:13 -0700

    added pause play menu screen

---
+++ Assets/Scripts/Level_0/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject puaseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if(GamePaused) {
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }
+    }
+    public void Resume() {
+        puaseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+
+    }
+    public void Pause() {
+        puaseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+
+    }
+
+    public void Home() {
+        puaseMenuUI.SetActive(false);
+    }
+}
