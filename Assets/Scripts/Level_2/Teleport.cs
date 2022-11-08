e5b2900fffff288dedf3ff913c39d6bb7f0dfd1f
Paluck Doshi <paluckdoshi@Palucks-MacBook-Air.local>
2022-11-07 16:07:00 -0800

    Final Portals added

---
+++ Assets/Scripts/Level_2/Teleport.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    
+
+    public GameObject portal1;
+    public GameObject portal2;
+    public GameObject Entry;
+
+    private GameObject player;
+    
+    public System.Random ran = new System.Random();
+    public int x;
+ 
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.tag == "Player")
+        {
+            x = ran.Next(0, 13);
+            if ((x%2) == 0)
+            {
+                player.transform.position = new Vector2(portal1.transform.position.x, portal1.transform.position.y);
+                Destroy(Entry);
+
+            }
+            else
+            {
+                player.transform.position = new Vector2(portal2.transform.position.x, portal2.transform.position.y);
+                Destroy(Entry);
+            }
+        }
+        Destroy(portal1);
+        Destroy(portal2);
+        
+    }
+}
